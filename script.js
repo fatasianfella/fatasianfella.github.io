f9903c734c15270b5dc17f602f75c6446c0532ed
FatAsianFella <fatachiggaseason@gmail.com>
2025-04-06 16:28:11 -0700

    add how to buy video

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-let num1, num2, operator, correctAnswer;
+const fatchiggaseasonpromo = document.getElementById('fatchiggaseasonpromo');
+const howtobuychga = document.getElementById('howtobuychga');
 
 function fadeOutWithTransition(element, duration) {
   element.style.transition = `opacity ${duration / 1000}s`;
@@ -23,9 +24,9 @@ function fadeToNextPage() {
   fadeOutWithTransition(document.getElementById('page1'), 1000);
   fadeOutWithTransition(document.getElementById('menacingFellowVideo'), 1000);
   document.getElementById('menacingFellowVideo').classList.add("hide");
-  document.getElementById('fatchiggaseasonpromo').classList.remove("hide");
-  document.getElementById('fatchiggaseasonpromo').style.opacity = 0.9;
-  document.getElementById('fatchiggaseasonpromo').style.zIndex = 10;  
+  fatchiggaseasonpromo.classList.remove("hide");
+  fatchiggaseasonpromo.style.opacity = 0.9;
+  fatchiggaseasonpromo.style.zIndex = 10;  
   document.getElementById('dexscreener').style.opacity=1;
   document.getElementById('dexscreener').style.zIndex=11;
   document.getElementById('instagramLink').style.opacity=1;
@@ -41,21 +42,20 @@ function fadeToNextPage() {
 
 function nextPage() {
   setTimeout(fadeToNextPage, 500);
-  setTimeout(document.getElementById("fatchiggaseasonpromo").play(),2000);
+  setTimeout(fatchiggaseasonpromo.play(),2000);
 }
 
-const video = document.getElementById('fatchiggaseasonpromo');
-video.addEventListener('ended', function() {
-  setTimeout(video.play(),4000);
+fatchiggaseasonpromo.addEventListener('ended', function() {
+  setTimeout(fatchiggaseasonpromo.play(),4000);
 });
 
 function volumeClick() {
   const volumeButton = document.getElementById('volumeControl');
-  if(video.muted) {
-    video.muted = false; 
+  if(fatchiggaseasonpromo.muted) {
+    fatchiggaseasonpromo.muted = false; 
     volumeButton.src="media/img/volume_up.png"
   } else {
-    video.muted = true; 
+    fatchiggaseasonpromo.muted = true; 
     volumeButton.src="media/img/volume_down.png"
   }
 }
@@ -63,4 +63,24 @@ function volumeClick() {
 function copyContactAddress() {
   navigator.clipboard.writeText("5SkgLR8nSZS7XwhhweFRHzKnQ3xaj4WAe8iRVGEnpump");
   alert("CA address copied");
+  fatchiggaseasonpromo.play();
+}
+
+var modal = document.getElementById('myModal');
+
+var span = document.getElementsByClassName("close")[0];
+
+function clickBuyNowButton() {
+  modal.style.display = "block";
+  howtobuychga.play();
+}
+
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+if (event.target == modal) {
+    modal.style.display = "none";
+}
 }
